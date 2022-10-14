32ebb117b0d602412dd6108cb140fb137af32252
Phan Vo Trong <trongtrong099@gmail.com>
2022-10-14 09:50:04 +0700

    2

--- views/FlatListBook.js
+++ views/FlatListBook.js
@@ -4,7 +4,16 @@ import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 export default function FlatListBook() {
     return (
         <View>
-
+             <FlatList
+            data={list}
+            renderItem={({item}) => 
+                <View>
+                    <Text>{item.todo}</Text>
+                    <TouchableOpacity
+                    onPress={() => {setList(list.filter( ite => ite!==item ));}}
+                    ><Text>Delete</Text></TouchableOpacity>
+                </View>}           
+            />
         </View>
     )
 }
